2bc274716093d37c6cddeb8b7e68913c24896f1b
hira29 <dzakydio@gmail.com>
2020-08-24 03:54:29 +0700

    first-commit

---
+++ src/pages/formpage.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class FormPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return(
+            <div>
+                Form Page
+            </div>
+        )
+    }
+}
+
+export default FormPage;
---
+++ src/pages/productpage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ProductPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Products
+            </div>
+        );
+    }
+}
+
+export default ProductPage;
